ac05f0e76bd791c319867fe9f879615ce39ca74f
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2018-07-04 19:15:58 -0700

    selecting multiple random numbers

--- pr23.hs
+++ pr23.hs
@@ -5,13 +5,23 @@ import Control.Monad (replicateM) -- replM n f applies f n times
 -- so more or less we use replM to apply the IO monad contained random number
 -- generator n times
 
--- rndSel :: [a] -> Int -> [a]
--- rndSel xs n =
---     let
---         l = length xs
---         s = newStdGen -- uses splitting behind, not nec. crypt. secure
---     in
+-- getElm :: IO Int -> [a] -> IO a
+-- getElm 0 (x:xs) = return x
+-- getElm n (x:xs) = getElm (n-1) xs
 
+rand :: Int -> Int -> IO Int
+rand x y = getStdRandom $ randomR (x, y) -- use splitting, not nec. stat. random
+
+rands :: Int -> Int -> Int -> IO [Int]
+rands x y n = replicateM n (rand x y)
+
+-- rndSel :: [a] -> Int -> IO [a]
+-- rndSel _ 0 = return []
+-- rndSel xs n =
+--     rands 0 l n >>= (\i -> return [xs !! i])
+--     where l = (length xs) - 1
+--
+--
 -- randElm :: [a] -> [IO a]
 -- randElm [] = return []
 -- randElm xs =
@@ -20,7 +30,3 @@ import Control.Monad (replicateM) -- replM n f applies f n times
 --         y = getStdRandom rr
 --     in
 --     y >>= (\z -> xs !! z)
-
-getElm :: IO Int -> [a] -> IO a
-getElm 0 (x:xs) = IO x
-getElm n (x:xs) = getElm (n-1) xs
