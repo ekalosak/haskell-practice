6b2293a4c1bdf56a7c25d30e2cadbdd1b5d516b6
ekalosak <ekalosak@gmail.com>
2018-07-26 10:45:47 -0700

    solved coprime  determination

---
+++ pr33.hs
@@ -0,0 +1,18 @@
+-- cop 4 6 -> True
+-- cop 4 9 -> False
+-- determine whether two integers are coprime i.e. if their gcd is 1 or not
+
+-- from pr32:
+
+g :: Int -> Int -> Int
+g 0 n = n
+g n 0 = n
+g a b =
+    let
+        x = max a b
+        y = min a b
+    in
+        g (x-y) y
+
+c :: Int -> Int -> Bool
+c a b = g a b == 1
