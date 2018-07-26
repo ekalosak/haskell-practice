0c1614c6a6cfedfa77fd6a93c3c6ab4c50a4ad67
ekalosak <ekalosak@gmail.com>
2018-07-26 12:21:45 -0700

    solved prime factorization problem

--- Pr36.hs
+++ Pr36.hs
@@ -5,14 +5,21 @@ import Pr35
 
 pfacm :: Int -> [(Int, Int)]
 pfacm 0 = []
--- pfacm n =
---     let
---         ps = pfacs' n
---         s = length [x | x <- ps, x == head ps] -- e.g. 100 -> [2,2,5,5] -> [2,2]
---     in
---         (head ps, s) : (pfacm . head . snd) $ splitAt s ps
+pfacm n =
+    let
+        ps = pfacs' n
+        ups = unique ps
+    in
+        countEach ups ps
 
 -- count 'a' "aabbc" -> 2
 count :: Eq a => a -> [a] -> Int
 count _ [] = 0
 count x ys = length [z | z <- ys, z == x]
+
+-- countEach "ab" "aabbc" -> [('a', 2), ('b', 2)]
+countEach :: Eq a => [a] -> [a] -> [(a, Int)]
+countEach [] _ = []
+countEach _ [] = []
+countEach [x] ys = [(x, count x ys)]
+countEach (x:xs) ys = (x, count x ys) : countEach xs ys
