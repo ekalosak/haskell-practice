28c6b2e5414d72ed3dfee45b8cb8707d6f3d86e2
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2018-07-26 00:04:18 -0700

    about to delete old attempt for new aproach

--- pr27.hs
+++ pr27.hs
@@ -8,18 +8,18 @@ g [1] [x] = [[[x]]]
 g [1] (x:xs) = [[x]] : g [1] xs
 g [n] [x] = [[[]]] -- n > 1 by failure to match preceeding patern
 
-g [n] (x:xs)
+g [n] (x:xs) -- subsets of size n from ys
     | n > length ys     = [[[]]]
     | n == length ys    = [[xs]]
     | otherwise         =   -- n < length xs e.g. >>> g 2 "asdf"
-        h x (g [n-1] xs) ++ g [n] xs
+        -- h x (g [n-1] xs) ++ g [n] xs -- THIS is wrong
     where ys = (x:xs)
 
--- h ["a"] [["bc"], ["bd"]] -> [["a", "bc"], ["a", "bd"]]
-h :: [[a]] -> [[[a]]] -> [[[a]]]
-h _ [[[]]] = [[[]]]
-h [[]] _ = [[[]]]
-h [x] [[y]] = [[x], [y]]
+-- -- h 'a' [["bc"], ["bd"]] -> [["a", "bc"], ["a", "bd"]]
+-- h :: a -> [[[a]]] -> [[[a]]]
+-- h _ [[[]]] = [[[]]]
+-- h x [y] = [[x]:y]
+-- h x [y:ys] = (h x [y]) ++ (h x [ys])
 
 -- for all permutations up to equality within subset permutations, take 1 then 2
 -- i.e. g [1,2] "abc" -> take 1 2 ["abc", "bac", "cab"]
