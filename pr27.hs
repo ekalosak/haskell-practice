2e41a6068f5cfb8a72b81a8d50c7c94b426c3423
ekalosak <ekalosak@gmail.com>
2018-07-26 10:35:50 -0700

    cleaned slate, starting again...

--- pr27.hs
+++ pr27.hs
@@ -3,21 +3,7 @@ g :: [Int] -> [a] -> [[[a]]]
 
 g [] _ = [[[]]]
 g _ [] = [[[]]]
--- g ns xs = split ns (perm xs)
 
--- take e.g. 2 from list, then apply recursively to leftovers
-
--- h 2 "abc" -> ["ab" "c", "bc" leftovers, "ac" leftovers]
-h :: Int -> [a] -> [([a], [a])]
-h 0 _ = []
-h 1 (x:xs) = ([x], xs) : h 1
-
--- splitAt 2 [1..5] -> [1,2] [3,4,5]
-
--- get all ~permutations of base string
--- for each of those, split at e.g. [1,2]
-
--- perm :: [a] -> [[a]]
--- perm [] = []
--- perm [x] = [x]
--- perm (x:xs) = 
+-- c gets list of combinations of elements and the elements excluded therefrom
+c :: Int -> [a] -> [([a], [a])]
+c 0 xs = [([],xs)]
