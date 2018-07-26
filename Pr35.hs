92bad7c84441e0b71547f650a5b71f0eaaf3700e
ekalosak <ekalosak@gmail.com>
2018-07-26 12:03:58 -0700

    solved with uniqueness the pfac problem

--- Pr35.hs
+++ Pr35.hs
@@ -5,13 +5,21 @@ import Pr31 -- isqrt, isprime
 import Pr33 -- c -- coprime
 import Pr32 -- g -- gcd
 
-pfacs :: Int -> [Int]
-pfacs n =
+pfacs' :: Int -> [Int]
+pfacs' n =
     if isprime n then [n] else
     let
         ps = takeWhile (<= (isqrt n)) primes
         p = head $ filter (\x -> not (c x n)) ps
-    in p:(pfacs $ div n p)
+    in p:(pfacs' $ div n p)
 
 -- usage e.g. take 4 primes -> [2,3,5,7]
 primes = filter isprime [2..]
+
+pfacs :: Int -> [Int]
+pfacs n = (unique . pfacs') n
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique [x] = [x]
+unique (x:xs) = x : unique [y | y <- xs, y /= x]
