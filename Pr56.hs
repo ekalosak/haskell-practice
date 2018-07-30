c413049610b453f75f3b2f903cdbbc787111f247
ekalosak <ekalosak@gmail.com>
2018-07-30 16:02:40 -0700

    solved tree symmetry testing

--- Pr56.hs
+++ Pr56.hs
@@ -10,3 +10,7 @@ mirror x y =
     if (value x) == (value y)
     then and [mirror (rchild x) (lchild y), mirror (lchild x) (rchild y)]
     else False
+
+sym :: Eq a => Tree a -> Bool
+sym Empty = True
+sym x = mirror (rchild x) (lchild x)
