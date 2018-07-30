0380c72cfdaccde6d37fdd952fa21d97d02ddfbf
ekalosak <ekalosak@gmail.com>
2018-07-30 12:22:53 -0700

    added stringcounting function as helper method

--- Pr50.hs
+++ Pr50.hs
@@ -4,12 +4,20 @@
 -- huff "go go gophers" -> [('g', "00"), ('o', "01"), (' ', "110"),
 -- ('p', "111"), ('h', "100") ..]
 
-count :: Eq a => [a] -> [(a, Int)]
-count [] = []
-
 huff :: Eq a => [a] -> [(a, [Char])]
 huff [] = []
 
+-- count "aaabbba" -> [('a', 3), ('b', 3), ('a', 1)]
+count :: Eq a => [a] -> [(a, Int)]
+count [] = []
+count [x] = [(x, 1)]
+count (x:xs) =
+    if x == (fst . head) cxs
+    then (x, (snd . head) cxs + 1):(tail cxs)
+    else (x, 1):cxs
+    where cxs = count xs
+
+-- sequential sort i.e. O(n^2)
 sort :: Ord a => [a] -> [a]
 sort [] = []
 sort [x] = [x]
