736df451308c79bbfc5028ce9e84e54254172209
ekalosak <ekalosak@gmail.com>
2018-08-01 14:00:37 -0700

    commented sections

--- tictactoe/Tictactoe.hs
+++ tictactoe/Tictactoe.hs
@@ -1,3 +1,4 @@
+-- Author: Eric Kalosa-Kenyon
 module Tictactoe where
 
 import Data.Matrix
@@ -6,11 +7,13 @@ import Control.Monad
 data Player = X | O | E deriving (Show, Read, Eq)
 type Board = Matrix Player
 
+-- define initial variables
 dim = (3, 3) -- NOTE: This should be treated as hardcoded - the winchecking
             -- methods below rely on 3x3 board
 
 board = fromList (fst dim) (snd dim) (iterate id E)
 
+-- board status calculation subroutines
 -- put (w) on (b) at (x, y)
 put :: Board -> (Int, Int) -> Player -> Board
 put b ix p = setElem p ix b
@@ -18,9 +21,9 @@ put b ix p = setElem p ix b
 full :: Board -> Bool
 full b = not . any (==E) $ b
 
+-- endgame conditions
 -- did player (p) win on board (b)?
 won, wonMainDiag, wonAltDiag, wonRows, wonCols :: Player -> Board -> Bool
--- TODO
 winconds = [wonMainDiag, wonAltDiag, wonRows, wonCols]
 won p b = any (==True) [wincond p b | wincond <- winconds]
 
@@ -32,10 +35,12 @@ wonCols p b = any (==True) [wonCol p b i | i <- [1..(fst dim)]]
 wonRow p b i = all (==p) $ getRow i b
 wonCol p b i = all (==p) $ getCol i b
 
+-- user input handling, IO monad stuff
 getIndex inp = read inp :: (Int, Int)
 getUserIndex = fmap getIndex getLine
-
 boardOutIO = putStrLn . show
+
+-- main loop
 main = boardOutIO board >>
     putStrLn "Place your X with format: (Int,Int)" >>
     getUserIndex >>=
