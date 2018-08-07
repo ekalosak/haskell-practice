5ad074814d6c387509756e9bbdb2640a4f38a1bd
ekalosak <ekalosak@gmail.com>
2018-08-07 12:55:34 -0700

    solved pr61

---
+++ Pr61.hs
@@ -0,0 +1,21 @@
+-- count leaves
+
+module Pr61 where
+-- module Pr61 (nleaves) where
+import Tree (Tree(Branch, Empty), leaf, lchild, rchild)
+
+nleaves :: Tree a -> Int
+nleaves = length . filter is_leaf . nodes
+
+nodes :: Tree a -> [Tree a]
+nodes Empty = []
+nodes tr = tr:((nodes $ lchild tr) ++ (nodes $ rchild tr))
+
+children :: Tree a -> [Tree a]
+children Empty = []
+children (Branch _ lc rc) = [lc, rc]
+
+is_leaf :: Tree a -> Bool
+-- is_leaf = and . (==Empty) <$> children
+is_leaf (Branch _ Empty Empty) = True
+is_leaf _ = False
--- Tree.hs
+++ Tree.hs
@@ -4,18 +4,18 @@ module Tree (Tree(Empty, Branch), leaf, lchild, rchild, value, depth, nnodes) wh
 data Tree a = Empty | Branch a (Tree a) (Tree a)
               deriving (Show)
 
--- -- leaf 'x' == leaf 'y' i.e. (==) respects only structure
--- instance Eq (Tree a) where
---     Empty == Empty = True
---     Branch x lc rc == Branch x2 lc2 rc2 = (and [lc == lc2, rc == rc2])
---     _ == _ = False
-
--- leaf 'x' == leaf 'x' i.e. force value equality testing when possible
-instance Eq a => Eq (Tree a) where
+-- leaf 'x' == leaf 'y' i.e. (==) respects only structure
+instance Eq (Tree a) where
     Empty == Empty = True
-    Branch x lc rc == Branch x2 lc2 rc2 =
-        (and [x == x2, lc == lc2, rc == rc2])
+    Branch x lc rc == Branch x3 lc2 rc2 = (and [lc == lc2, rc == rc2])
     _ == _ = False
+
+-- -- leaf 'x' == leaf 'x' i.e. force value equality testing when possible
+-- instance Eq a => Eq (Tree a) where
+--     Empty == Empty = True
+--     Branch x lc rc == Branch x2 lc2 rc2 =
+--         (and [x == x2, lc == lc2, rc == rc2])
+--     _ == _ = False
 
 leaf x = Branch x Empty Empty
 
