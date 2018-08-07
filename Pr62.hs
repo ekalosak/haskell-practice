d7f26bd841c81be9f467118ebe71e0f8e52f60ff
ekalosak <ekalosak@gmail.com>
2018-08-07 13:34:42 -0700

    solved pr6a

--- Pr62.hs
+++ Pr62.hs
@@ -1,7 +1,13 @@
 -- get internal nodes
 
 module Pr62 where
-import Tree (Tree(Branch, Empty), is_leaf, nodes, leaf)
+import Tree (Tree(Branch, Empty), is_leaf, nodes, leaf, children)
 
 internal_nodes :: Tree a -> [Tree a]
 internal_nodes = filter (not . is_leaf) . nodes
+
+nodes_at :: Int -> Tree a -> [Tree a]
+nodes_at _ Empty = []
+nodes_at 1 tr = [tr]
+nodes_at n tr = (>>=) (children tr) (nodes_at (n-1))
+-- nodes_at n = (\x -> (children x >>= (nodes_at (n-1))))
