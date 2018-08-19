57e03ca5ae83aacd51d009213ebe02f28de9b506
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2018-08-19 07:36:47 -0700

    rewrote 64. still not solved but progress.

--- Pr64.hs
+++ Pr64.hs
@@ -4,10 +4,21 @@
 module Pr64 (lay) where
 import Tree(Tree(Branch, Empty), leaf, depth, min_depth, nnodes)
 
-lay :: Tree a -> Tree (a, (Int, Int))
-lay Empty = Empty
--- lay (Branch x lc rc) = Branch (x, )
--- lay (Branch x lc rc) = Branch (x, )
+-- -- external use function
+layout :: Tree a -> Tree (a, (Int, Int))
+layout = lay 0 0
+
+-- take a tree at depth n and mark each node with y and x location
+-- *helper function*
+lay :: Int -> Int -> Tree a -> Tree (a, (Int, Int))
+lay _ _ Empty = Empty
+lay d w (Branch x lc rc) =
+    Branch
+        (x, (d+1, (width lc) + 1))
+        (lay (d+1) lc)
+        (lay (d+1) rc)
+
+-- NOTE: This solution does not yet work
 
 -- width of a tree is just the number of nodes in it for this layout
 width :: Tree a -> Int
