22a7956813627d53a41d2638093a47ab4e95d963
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2018-08-22 21:15:33 -0700

    solved pr64

--- Pr64.hs
+++ Pr64.hs
@@ -10,16 +10,21 @@ layout = lay 0 0
 
 -- take a tree at depth n and mark each node with y and x location
 -- *helper function*
+-- include parent's x and y
 lay :: Int -> Int -> Tree a -> Tree (a, (Int, Int))
 lay _ _ Empty = Empty
-lay d w (Branch x lc rc) =
-    Branch
-        (x, (d+1, (width lc) + 1))
-        (lay (d+1) lc)
-        (lay (d+1) rc)
-
--- NOTE: This solution does not yet work
+lay d w (Branch x lc rc) = let w2 = (width lc) + w in
+    (Branch
+        (x, (d, w2))
+        (lay (d+1) w lc)
+        (lay (d+1) (w2+1) rc)
+    )
 
 -- width of a tree is just the number of nodes in it for this layout
 width :: Tree a -> Int
 width = nnodes
+
+test_tree = Branch 'x'
+        (Branch 'y' (leaf 'c') Empty)
+        (Branch 'z' (leaf 'a') (leaf 'b'))
+-- *Pr64> layout test_tree
