51929d3aba95a58f18d4f93555fde6c301199be0
ekalosak <ekalosak@gmail.com>
2018-08-27 11:15:16 -0700

    redrafted second layout problem, still not finished

--- Pr65.hs
+++ Pr65.hs
@@ -1,25 +1,26 @@
 -- equidistant neighbors in layout
 
-module Pr65 (layout) where
-import Tree(Tree(Branch, Empty), leaf, depth, min_depth, nnodes)
+-- module Pr65 (layout) where
+module Pr65 where
+import Tree(Tree(Branch, Empty), leaf, depth, lchild)
 
 -- -- external use function
-layout :: Tree a -> Tree (a, (Int, Int))
-layout = lay 0 0
+-- layout :: Tree a -> Tree (a, (Int, Int))
+-- layout = (\t -> lay (getrootyx t) t)
 
--- take a tree at depth n and mark each node with y and x location
--- *helper function*
--- include parent's x and y
-lay :: Int -> Int -> Tree a -> Tree (a, (Int, Int))
-lay _ _ Empty = Empty
-lay d w (Branch x lc rc) = let w2 = 2^(max (depth lc) (depth rc)) in
-    (Branch
-        (x, (d, w2 + w))
-        (lay (d+1) (div (w2+w) 2) lc)
-        (lay (d+1) (3*(div (w2+w) 2)) rc)
-    )
+getrootyx :: Tree a -> (Int, Int)
+getrootyx t = (depth t, 2^((getlwidth t) - 1))
 
-test_tree = Branch 'x'
-        (Branch 'y' (leaf 'c') Empty)
-        (Branch 'z' (leaf 'a') (leaf 'b'))
+getlwidth :: Tree a -> Int
+getlwidth Empty = 0
+getlwidth t = 2^(depth t - 1) + (getlwidth (lchild t))
+
+test_tree =
+    Branch 'x'
+        (Branch 'y'
+            (leaf 'c')
+            Empty)
+        (Branch 'z'
+            (leaf 'a')
+            (leaf 'b'))
 -- *Pr64> layout test_tree
