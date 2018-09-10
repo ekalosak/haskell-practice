87acb58495e6d1e8d8dfc42e75ce532ff43fa2ee
ekalosak <ekalosak@gmail.com>
2018-09-10 11:33:29 -0700

    aesthetic adjustments for tree layout to normalize x and y relative to (1,1)

--- Pr66.hs
+++ Pr66.hs
@@ -3,15 +3,45 @@
 module Pr66 (layout) where
 import Tree(Tree(Branch, Empty), leaf, depth, lchild, value)
 
--- external use function
-
-layout :: Tree a -> Tree ((Int, Int), a)
-layout Empty = Empty
-
 -- Strategy:
 -- 1. initialize x and y values
 -- 2. look for conflicts in locations
 -- 3. adjust conflicts' x values at deepest common parent
+
+
+layout :: Tree a -> Tree ((Int, Int), a)
+-- TODO: The following is not correct. include the conflict adjusting recursive
+-- functions.
+layout = finaladj . layxinit . layy
+
+finaladj = finalyadjust . finalxadjust
+
+-- move y so that miny is 1
+finalyadjust :: Tree ((Int, Int), a) -> Tree ((Int, Int), a)
+finalyadjust Empty = Empty
+finalyadjust tr = adjy 1 tr
+
+-- move x so that minx is 1
+finalxadjust :: Tree ((Int, Int), a) -> Tree ((Int, Int), a)
+finalxadjust Empty = Empty
+finalxadjust tr = adjx (minxt tr) tr
+
+minxt :: Tree ((Int, Int), a) -> Int
+minxt = foldl1 min . minxth
+
+minxth :: Tree ((Int, Int), a) -> [Int]
+minxth Empty = []
+minxth (Branch ((x, y), k) lc rc) = x:((minxth lc) ++ (minxth rc))
+
+adjx :: Int -> Tree ((Int, Int), a) -> Tree ((Int, Int), a)
+adjx _ Empty = Empty
+adjx d (Branch ((x, y), k) lc rc) =
+    (Branch ((x+d, y), k) (adjx d lc) (adjx d rc))
+
+adjy :: Int -> Tree ((Int, Int), a) -> Tree ((Int, Int), a)
+adjy _ Empty = Empty
+adjy d (Branch ((x, y), k) lc rc) =
+    (Branch ((x, y+d), k) (adjy d lc) (adjy d rc))
 
 -- layout the y values
 layy = layyh 0
@@ -28,6 +58,8 @@ tolist (Branch x lc rc) = x:((tolist lc) ++ (tolist rc))
 
 -- initialize x value
 layxinit = layxinith 0
+
+-- x layout helper
 layxinith :: Int -> Tree (Int, a) -> Tree ((Int, Int), a)
 layxinith _ Empty = Empty
 layxinith x (Branch (y, k) lc rc) =
