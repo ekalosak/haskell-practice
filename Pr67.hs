538d9a2f03f57504bbd9b159d6d29d7dd46a86cf
ekalosak <ekalosak@gmail.com>
2018-09-10 14:07:54 -0700

    framework for Pr67 parsing string to tree, not really all that interested in parsing

--- Pr67.hs
+++ Pr67.hs
@@ -1,7 +1,7 @@
 -- string to and from tree
 
 module Pr67 (tostr) where
-import Tree(Tree(Branch, Empty), test_tree)
+import Tree(Tree(Branch, Empty), test_tree, leaf)
 
 -- tostr :: Show a => Tree a -> [Char]
 -- tostr Empty = []
@@ -22,3 +22,18 @@ tostr (Branch x Empty rc) =
     x:("(," ++ (tostr rc) ++ ")")
 tostr (Branch x lc rc) =
     x:("(" ++ (tostr lc) ++ "," ++ (tostr rc) ++ ")")
+
+totree :: [Char] -> Tree Char
+totree (x:[]) = leaf x
+totree (x:xs) = let (lpar, rpar) = parsplit xs in
+    Branch x (totree $ lpar) (totree $ rpar)
+    -- Branch x Empty Empty
+
+parsplit :: [Char] -> ([Char], [Char])
+-- TODO
+parsplit str =
+    if head str == '(' && last str == ')'
+    then ("asdf", "qwer")
+    else error $ "Parse error: <" ++ str ++ ">"
+
+test_string = "a(b(c,d),e(f,))"
