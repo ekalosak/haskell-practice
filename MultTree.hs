85365e8b6e8d5a20ded0bdf8077895df5d6d5b4f
ekalosak <ekalosak@gmail.com>
2018-09-10 14:36:18 -0700

    solved pr 70B i.e. counting nodes

--- MultTree.hs
+++ MultTree.hs
@@ -1,3 +1,29 @@
-module MultTree (Tree(Node)) where
+-- module containing multi-child trees and accessory functions
+module MultTree (Tree(Node), leaf) where
+-- module MultTree (Tree(Node), leaf, nnodes) where
 
+-- data types
 data Tree a = Node a [Tree a] deriving (Show, Eq)
+
+-- helper functions
+leaf x = Node x []
+
+nnodes :: Tree a -> Int
+nnodes = sum1 . map nnodes . children
+
+sum1 :: (Foldable t, Num a) => t a -> a
+sum1 xs = 1 + sum xs
+
+children :: Tree a -> [Tree a]
+children (Node x xs) = xs
+
+-- test tree source: https://wiki.haskell.org/99_questions/70B_to_73
+-- under problem 70B
+test_tree = Node 'a' [
+                Node 'f' [Node 'g' []],
+                Node 'c' [],
+                Node 'b' [Node 'd' [], Node 'e' []]
+                ]
+
+-- instance Show a => Show (Tree a) where
+--     show (Node x xs) = x ++ "\n" ++ (flatten $ map show xs)
