ff85a88d604d526dc46fa941ca61122d195a715f
ekalosak <ekalosak@gmail.com>
2018-09-10 17:20:02 -0700

    wrote unique-getting function

--- Graph.hs
+++ Graph.hs
@@ -1,7 +1,6 @@
-module Graph (Graph, Node) where
--- module Graph (Graph(Node, Empty)) where
--- module Graph (Graph(Empty, Node, Edge)) where
+module Graph (Graph, Node, unique) where
 
+-- -- DATA CONSTRUCTORS
 data Node a = Node a deriving Eq
 -- graph-term form
 data Graph a = Graph [Node a] [(Node a, Node a)] deriving Eq
@@ -14,6 +13,20 @@ instance Show a => Show (Node a) where
 instance Show a => Show (Graph a) where
     show (Graph nodes edges) = show edges
 
+-- -- FUNCTIONS
+convert_gtoe :: Graph a -> GraphE a
+convert_gtoe (Graph nodes edges) = (GraphE edges)
+
+-- convert_etog :: GraphE a -> Graph a
+-- convert_etog (GraphE edges) =
+--     let nodes = unique $ extract_nodes edges in
+--     Graph nodes edges
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (x:xs) = x:(unique [y | y <- xs, y /= x])
+
+-- -- TESTING COMPONENTS
 x = Node 'x'
 y = Node 'y'
 e = (x, y)
