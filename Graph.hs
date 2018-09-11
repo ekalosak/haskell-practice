17a4671b239125d36ebf300825f5128f4e98b2f4
ekalosak <ekalosak@gmail.com>
2018-09-10 17:37:40 -0700

    added another graph form, renamed Graph to GraphG to respect the notion that it's a particular form

--- Graph.hs
+++ Graph.hs
@@ -1,35 +1,39 @@
-module Graph (Graph, Node, unique) where
+module GraphG (GraphG, Node, unique) where
 
 -- -- DATA CONSTRUCTORS
 data Node a = Node a deriving Eq
 -- graph-term form
--- data GraphG a = Graph a | GraphE a
-data Graph a = Graph [Node a] [(Node a, Node a)] deriving Eq
+data GraphG a = GraphG [Node a] [(Node a, Node a)] deriving Eq
 -- edge-term form
 data GraphE a = GraphE [(Node a, Node a)] deriving (Show, Eq)
+-- adjacency form
+data GraphA a = GraphA [(Node a, [Node a])] -- list of (node, connected nodes)
+-- TODO: convert between GraphA and the other two
+-- TODO: validation functions for these forms
 
 instance Show a => Show (Node a) where
     show (Node x) = show x
 
-instance Show a => Show (Graph a) where
-    show (Graph nodes edges) = "Graph <" ++ show edges ++ ">"
+instance Show a => Show (GraphG a) where
+    show (GraphG nodes edges) = "GraphG: \n\t" ++
+        show nodes ++ "\n\t" ++ show edges
 
 -- -- FUNCTIONS
 -- conversions
-convert_gtoe :: Graph a -> GraphE a
-convert_gtoe (Graph nodes edges) = (GraphE edges)
+convert_gtoe :: GraphG a -> GraphE a
+convert_gtoe (GraphG nodes edges) = (GraphE edges)
 
-convert_etog :: Eq a => GraphE a -> Graph a
+convert_etog :: Eq a => GraphE a -> GraphG a
 convert_etog (GraphE edges) =
     let nodes = unique $ extract_nodes edges in
-    Graph nodes edges
+    GraphG nodes edges
 
 -- basic functionality
-nodes :: Graph a -> [Node a]
-nodes (Graph ns es) = ns
+nodes :: GraphG a -> [Node a]
+nodes (GraphG ns es) = ns
 
-edges :: Graph a -> [(Node a, Node a)]
-edges (Graph ns es) = es
+edges :: GraphG a -> [(Node a, Node a)]
+edges (GraphG ns es) = es
 
 -- helpers
 extract_nodes :: [(Node a, Node a)] -> [Node a]
@@ -48,4 +52,4 @@ unique (x:xs) = x:(unique [y | y <- xs, y /= x])
 x = Node 'x'
 y = Node 'y'
 e = (x, y)
-g = Graph [x, y] [e]
+g = GraphG [x, y] [e]
