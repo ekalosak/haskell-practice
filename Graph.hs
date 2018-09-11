cd5f3b81c124d6a0828bae59e3784953d70f694e
ekalosak <ekalosak@gmail.com>
2018-09-10 17:29:08 -0700

    wrote some graph-form conversion functions and some basic functionality i.e. node and edge extraction

--- Graph.hs
+++ Graph.hs
@@ -3,6 +3,7 @@ module Graph (Graph, Node, unique) where
 -- -- DATA CONSTRUCTORS
 data Node a = Node a deriving Eq
 -- graph-term form
+-- data GraphG a = Graph a | GraphE a
 data Graph a = Graph [Node a] [(Node a, Node a)] deriving Eq
 -- edge-term form
 data GraphE a = GraphE [(Node a, Node a)] deriving (Show, Eq)
@@ -11,16 +12,33 @@ instance Show a => Show (Node a) where
     show (Node x) = show x
 
 instance Show a => Show (Graph a) where
-    show (Graph nodes edges) = show edges
+    show (Graph nodes edges) = "Graph <" ++ show edges ++ ">"
 
 -- -- FUNCTIONS
+-- conversions
 convert_gtoe :: Graph a -> GraphE a
 convert_gtoe (Graph nodes edges) = (GraphE edges)
 
--- convert_etog :: GraphE a -> Graph a
--- convert_etog (GraphE edges) =
---     let nodes = unique $ extract_nodes edges in
---     Graph nodes edges
+convert_etog :: Eq a => GraphE a -> Graph a
+convert_etog (GraphE edges) =
+    let nodes = unique $ extract_nodes edges in
+    Graph nodes edges
+
+-- basic functionality
+nodes :: Graph a -> [Node a]
+nodes (Graph ns es) = ns
+
+edges :: Graph a -> [(Node a, Node a)]
+edges (Graph ns es) = es
+
+-- helpers
+extract_nodes :: [(Node a, Node a)] -> [Node a]
+extract_nodes [] = []
+extract_nodes edges = flatten $ map (\t -> [fst t, snd t]) edges
+
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten (x:xs) = x ++ flatten xs
 
 unique :: Eq a => [a] -> [a]
 unique [] = []
